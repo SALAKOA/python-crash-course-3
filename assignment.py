aacd1eabb59e3f17144da858875dd5b8c8d1f937
Akeem Lagundoye <38335545+Aifedayo@users.noreply.github.com>
2021-11-06 11:12:54 +0100

    Update assignment.py
    Code LGTM

--- assignment.py
+++ assignment.py
@@ -1,11 +1,16 @@
 my_favourite_food=("rice","yam","eba")
 print (my_favourite_food)
+
 The_number_of_letter_in_my_name=10
 print(The_number_of_letter_in_my_name)
+
 my_age=36
 print(my_age/19)
-2<9
-14>100
+
+print(2<9)
+
+print(14>100)
+
 # above is task 3
 # below is task 2
 #area of rectangle is L*B
@@ -13,26 +18,31 @@ length=8
 width= 5
 area=length*width
 print(area)
+
 # Area of circle
 pi=3.142
 radius=7
 area_of_circle=(pi*radius**2)
 print(area_of_circle)
+
 # volume of cylinder
 depth=20
 radius=2.5
 volume_of_cylinder=(3.142*radius**2*20)
 print(volume_of_cylinder)
+
 #volume of a cuboid=(length*breadth*heigth)
 length=25
 height=5
 breadth=15
 volume_of_a_cuboid=(length*breadth*heigth)
 print(volume_of_a_cuboid)
+
 # area of triangle=1/2bh
 base1=8
 height1=12
 area_of_triangle=(base1*height1)
+
 #task4 below
 #addition
 num1=14
